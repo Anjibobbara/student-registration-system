032ac6be4b9cac8a9e7c1cbf79a630b0527f1427
Anjibobbara <anjib8790@gmail.com>
2026-07-14 14:26:58 +0530

    Fixed JWT authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,5 @@
+require("dotenv").config({ path: ".env.local" });
+
 const jwt = require("jsonwebtoken");
 
 
@@ -7,35 +9,56 @@ function verifyAdmin(req){
     const authHeader = req.headers.authorization;
 
 
+    console.log("AUTH SECRET CHECK:", process.env.JWT_SECRET);
+    console.log("AUTH HEADER:", authHeader);
+
+
+
     if(!authHeader){
+
         return false;
+
     }
+
 
 
     const token = authHeader.split(" ")[1];
 
 
+
+    if(!token){
+
+        return false;
+
+    }
+
+
+
     try{
+
 
         jwt.verify(
             token,
-            process.env.JWT_SECRET || "mySuperSecretKey12345"
+            process.env.JWT_SECRET
         );
 
 
         return true;
 
 
-    }catch(error){
+    }
+    catch(error){
+
 
         console.log("JWT ERROR:", error.message);
+
 
         return false;
 
     }
 
-
 }
 
 
-module.exports = verifyAdmin;
+
+module.exports = verifyAdmin;;
